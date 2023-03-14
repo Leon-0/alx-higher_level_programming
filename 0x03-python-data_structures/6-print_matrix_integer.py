efc9d2f157e9410729bd4eb56efc57c90a1b8ced
Leon-0 <36256754+Leon-0@users.noreply.github.com>
2023-03-14 01:13:15 +0100

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,6 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in range(len(row)):
-            # use str.format() to print integer in the required format
-            # by specifying the field width as 2 and aligning the text to the right
             print("{:2d}".format(row[i]), end=' ')
-        print()  # print newline after each row
+        print()
 
