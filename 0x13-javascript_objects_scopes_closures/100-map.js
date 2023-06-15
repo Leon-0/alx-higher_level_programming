5c0a482c2d31c94eb9a842105da41e5bdc350bdd
Leon-0 <36256754+Leon-0@users.noreply.github.com>
2023-06-16 00:45:34 +0100

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -4,6 +4,7 @@ const { list } = require('./100-data');
 
 const newList = list.map((value, index) => value * index);
 
-console.log("Initial list:", list);
-console.log("New list:", newList);
+console.log(list);
+console.log(newList);
+
 
