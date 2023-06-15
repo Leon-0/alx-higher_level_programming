fb52ac00f68657685bcefb26bf89b83fc8a738e2
Jesuferanmi Omoniyi <lyoncbe@gmail.com>
2023-06-16 00:09:57 +0100

    argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined) {
-  console.log('No argument');
+const args = process.argv.slice(2);
+const firstArgument = args[0];
+
+if (firstArgument) {
+  console.log(firstArgument);
 } else {
-  console.log(process.argv[2]);
+  console.log("No argument");
 }
