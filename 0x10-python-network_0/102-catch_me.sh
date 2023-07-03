a265c1dc577820d040037b7139d740372fd70414
Leon-0 <36256754+Leon-0@users.noreply.github.com>
2023-07-03 03:12:06 +0100

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -2,8 +2,5 @@
 # Make a PUT request to 0.0.0.0:5000/catch_me and include a custom header to trigger the desired response
 response=$(curl -s -X PUT -H "Referer: 0.0.0.0:5000/catch_me" -w "%{redirect_url}" 0.0.0.0:5000/catch_me)
 
-# Extract the redirected URL
 redirect_url=$(echo "$response" | sed 's/.*href="\([^"]*\)".*/\1/')
-
-# Make a GET request to the redirected URL
-curl -o /dev/tty "$redirect_url"
+curl -s "$redirect_url"
