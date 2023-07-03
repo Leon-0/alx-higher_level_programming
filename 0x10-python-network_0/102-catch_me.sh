eed17214158b20207d3f98a8c9f4d5d4031d5ef1
Leon-0 <36256754+Leon-0@users.noreply.github.com>
2023-07-03 02:50:13 +0100

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Make a POST request to 0.0.0.0:5000/catch_me and include a custom header to trigger the desired response
-curl -s -X POST -H "Referer: 0.0.0.0:5000/catch_me" 0.0.0.0:5000/catch_me >/dev/null
+curl -s -X PUT -H "Referer: 0.0.0.0:5000/catch_me" 0.0.0.0:5000/catch_me >/dev/null
