661d8ce3c9f141053a5b7d8072c6e6f7f9e74954
Leon-0 <36256754+Leon-0@users.noreply.github.com>
2023-07-03 02:43:05 +0100

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Makes a request to [::-1]/catch_me that causes the server to respond with a message
-response=$(curl -s -X PUT -d "user_id=98" -H "Referer: 0.0.0.0:5000/catch_me" -w "%{redirect_url}\n" 0.0.0.0:5000/catch_me)
-curl -s "$response"
+curl -s -L -X PUT -d "user_id=98" -H "Referer: 0.0.0.0:5000/catch_me" 0.0.0.0:5000/catch_me
